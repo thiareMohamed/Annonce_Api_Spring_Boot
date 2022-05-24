dea53440902c638bd6d277368ed3f4b3d7ff1f0b
thiaremohamed.mt@gmail.com <thiaremohamed.mt@gmail.com>
2022-05-24 12:03:35 +0000

    add Service AnnonceService

---
+++ src/main/java/sn/thiare/annonceapi/service/AnnonceService.java
@@ -0,0 +1,32 @@
+package sn.thiare.annonceapi.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import sn.thiare.annonceapi.entity.Annonce;
+import sn.thiare.annonceapi.repository.AnnonceRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AnnonceService {
+    private AnnonceRepository annonceRepository;
+
+    public AnnonceService(AnnonceRepository annonceRepository) {
+        this.annonceRepository = annonceRepository;
+    }
+
+    public List<Annonce> getAll(){
+        return annonceRepository.findAll();
+    }
+    public Optional<Annonce> getAllById(int id){
+        return annonceRepository.findById(id);
+    }
+    public List<Annonce> create(Annonce annonce){
+        return (List<Annonce>) annonceRepository.save(annonce);
+    }
+    public void delete(int id) {
+        annonceRepository.deleteById(id);
+    }
+
+}
